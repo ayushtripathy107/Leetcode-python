09f8a9c8b61e83f4f7845ddb60679f8a34a4705a
Ayush Tripathy <ayushtripathy07@gmail.com>
2026-05-19 11:27:09 +0530

    Implement valid parentheses checker in Python

---
+++ Questions/Vaild Parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # Map closing brackets to their corresponding opening brackets
+        bracket_map = {")": "(", "}": "{", "]": "["}
+        stack = []
+
+        for char in s:
+            if char in bracket_map:
+                # Pop the top element if stack isn't empty; otherwise use a dummy value
+                top_element = stack.pop() if stack else '#'
+                
+                # Check if the popped element matches the mapping
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # It's an opening bracket, push it onto the stack
+                stack.append(char)
+
+        # If the stack is empty, all brackets were matched correctly
+        return not stack
