ccbc6b2a3290d4415f5163c75f0e33bea2238516
Ayush Tripathy <ayushtripathy07@gmail.com>
2026-06-06 16:20:11 +0530

    Implement left-right sum difference calculation

---
+++ Left and Right Sum Difference.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def leftRightDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        leftSum = 0
+        rightSum = sum(nums)
+        answer = []
+        
+        for num in nums:
+            # Update rightSum to exclude the current element
+            rightSum -= num
+            
+            # Append the absolute difference
+            answer.append(abs(leftSum - rightSum))
+            
+            # Update leftSum to include the current element for the next index
+            leftSum += num
+            
+        return answer
