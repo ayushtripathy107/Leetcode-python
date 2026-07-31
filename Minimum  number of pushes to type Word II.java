466642ab9e61f9fb297a26a7261940e5dfaa3d45
Ayush Tripathy <ayushtripathy07@gmail.com>
2026-07-31 20:49:42 +0530

    Implement minimum pushes calculation for typing word

---
+++ Minimum  number of pushes to type Word II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] count = new int[26];
+        for (char c : word.toCharArray()) {
+            count[c - 'a']++;
+        }
+        java.util.Arrays.sort(count);
+        int total = 0;
+        int multiplier = 1;
+        int distinct = 0;
+        for (int i = 25; i >= 0; i--) {
+            if (count[i] == 0) break;
+            total += count[i] * multiplier;
+            distinct++;
+            if (distinct % 8 == 0) {
+                multiplier++;
+            }
+        }
+        return total;
+    }
+}
